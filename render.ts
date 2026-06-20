64a85eaeaf7ef748c8a6d6dbf566752aaf04daa0
Jeremy Tice <jetpacmonkey@gmail.com>
2026-06-20 12:09:32 -0400

    Load data from API

--- render.ts
+++ render.ts
@@ -1,12 +1,21 @@
+import AmbientWeatherApi from 'ambient-weather-api';
 import type { EnvValues } from './types.ts';
 
-export default async function render({
-  API_KEY,
-  APPLICATION_KEY,
-  DEVICE_ID,
-}: Readonly<EnvValues>): Promise<string> {
+let _api: AmbientWeatherApi;
+function getApi(env: Readonly<Pick<EnvValues, 'API_KEY' | 'APPLICATION_KEY'>>) {
+  return (_api ??= new AmbientWeatherApi({
+    apiKey: env.API_KEY,
+    applicationKey: env.APPLICATION_KEY,
+  }));
+}
+
+export default async function render(
+  env: Readonly<EnvValues>,
+): Promise<string> {
+  const api = getApi(env);
+  const data = await api.deviceData(env.DEVICE_ID, { limit: 288 });
   return `<html>
-test
+<pre>${JSON.stringify(data, undefined, 2)}</pre>
 </html>
 `;
 }
